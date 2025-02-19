7518b928a0d7acf878e33d3c812b21f1beac4088
Abhishek Kumar <akumar.12121999@gmail.com>
2025-02-19 11:53:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+let arr = [1,2,3,4,5];
+
+console.log(arr);
+
+const arr1 = ["Hello", "India"];
+let res = arr1.push["from"];
+console.log(arr1);
+
+
+let arr3 = ["Jarvis" , "The" , "Developer"];
+let rese = arr3.slice(1,2);
+console.log(rese);
+
+let arr4 = ["Jarvis" , "The" , "Developer"];
+let rese1 = arr3.toString();
+console.log(rese1);
+
+let arr5 = ["Jarvis" , "The" , "Developer"];
+let rese2 = arr3.shift();
+console.log(rese2);
+
+let arr6 = [1,2,3,4,5];
+let rese3 = arr6.map(x => x *2);
+console.log(rese3);
+
+let arr7 = ["Jarvis" , "The" , "Developer"];
+let rese4 = arr3.pop();
+console.log(rese4);
+
+let arr8 = ["Jarvis" , "The" , "Developer"];
+let rese5 = arr3.filter(word => word.length > 2);
+console.log(rese5);
+
+let arr9 = [1,2,3,4,5];
+let rese6 = arr9.find(x => x == 2);
+console.log(rese6);
+
+let arr10 = [1,2,3,4,5];
+let rese7 = arr10.findIndex(x => x == 5);
+console.log(rese7);
+
+let arr11 = [1,2,3,4,5];
+let rese8 = arr11.includes(7);
+console.log(rese8);
+
+let arr12 = ["Jarvis" , "The" , "Developer"];
+let rese9 = arr3.unshift(10);
+console.log(rese9);
